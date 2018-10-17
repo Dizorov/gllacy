c56a09d7e8bb61a8fb9a66b8b4cf71e6bade2b71
dizorov <dizorov666@gmail.com>
2018-10-17 16:41:16 +0300

    Небольшие изменения в файле popup.js

--- js/popup.js
+++ js/popup.js
@@ -5,9 +5,9 @@ var writeUs = document.querySelector('.modal-write-us__container_overlay'),
 writeUsOpener.addEventListener('click', function(evt) {
 	evt.preventDefault();
 	writeUs.classList.add('modal-write-us_is-showed');
-})
+});
 
 writeUsCloser.addEventListener('click', function(evt) {
 	evt.preventDefault();
 	writeUs.classList.remove('modal-write-us_is-showed');
-})
+});
